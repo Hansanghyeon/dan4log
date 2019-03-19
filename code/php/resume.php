a0d652591a426d40053d741cff0ce95e09fbebb3
Dan9509 <dandan9509@gmail.com>
2019-03-19 01:47:13 +0000

    새싹 애니메이션 추가, js css 등록

--- code/php/resume.php
+++ code/php/resume.php
@@ -4,133 +4,141 @@ Template Name: fullpage
 */
 get_header();
 ?>
-
-<div class="resumeHeader">
-    <div>
-        <h1>한상현</h1>
-        <h5>
-            <a href="mailto:dandan9509@gmail.com">dandan9509@gmail.com</a><br>
-            <a href="tel:010-5392-9950">010-5392-9950</a>
-        </h5>
+<section>
+    <div class="resumeHeader">
+        <div>
+            <h1>한상현</h1>
+            <h5>
+                <a href="mailto:dandan9509@gmail.com">dandan9509@gmail.com</a><br>
+                <a href="tel:010-5392-9950">010-5392-9950</a>
+            </h5>
+        </div>
+        <div class="profile">
+            <img src="https://s3-ap-northeast-2.amazonaws.com/4log-media/wp-content/uploads/20190314184237/profile_3ver.jpg">
+            <img src="https://s3-ap-northeast-2.amazonaws.com/4log-media/wp-content/uploads/20190314184237/M_Photo.jpg">
+        </div>
     </div>
-    <div class="profile">
-        <img src="https://s3-ap-northeast-2.amazonaws.com/4log-media/wp-content/uploads/20190314184237/profile_3ver.jpg">
-        <img src="https://s3-ap-northeast-2.amazonaws.com/4log-media/wp-content/uploads/20190314184237/M_Photo.jpg">
+    <div class="resumeSection1">
+        <h2>자기소개</h2>
+        <hr style="margin:20px 0px;">
+        <h2>코딩?</h2>
+        내가 원하는 것을만들어 줄 수 있게 해준 또 하나의 손 같은 존재입니다.<br>
+        에디터를 사용해서 내가 원하는 작업물을 시각적이 아닌 읽어서 만들어 낸다는 느낌은 저한테 정말 좋았습니다.<br>
+        컴퓨터를 좋아하는 저한테 적성도 정말 잘 맞았고 그런지 앉아서 몇 시간을 코딩을 해도 지루하지 않고<br>
+        이것저것 만져보면서 오류를 해결했을 때 그 기분은 매우 좋아서 저는 코딩을 놓지 못하겠습니다.<br>
+        지금의 계획은 Javascript를 좀더 숙련도있게 계속해서 프런트, 백앤드 모두를 움직일수있고 생각대로<br>
+        만들어낼수 있는 개발자까지 성장하고싶은 한 사람입니다.<br>
+        <br>
+        그럼 제가 코딩을 좋아하게된<br>
+        저의 짧은 인생사를 소개해볼께요!<br>
+        저의 나이는 95년생입니다. 어리다면 어릴수있지만 자신에 대해서 책임을 져야할 나이라고 생각합니다<br>
+        20살까지는 어느 목표가없는 평범한 학생이었습니다. 대학교진학은 학비의 부담을 느껴 포기하고 빠른 취업을 생각하고있었죠<br>
+        그렇게 21살에 입대를하고 군대에서 많은 것들이 저를 성숙하게 만들어줬습니다.<br>
+        22살 겨울에 재대를하고 저는 UIUX디자인(국비훈련) 받게되었습니다.<br>
+        디자인을 고등학교 방송부때부터 해왔는데 그 디자인을 텍스트로 하나하나 만드는 재미<br>
+        블로그를 운영하는 것에있어서 엄청난 확장성을 느껴서<br>
+        그때부터 웹개발자가 되야겠다라고 마음을 먹었습니다.<br>
+        <br>
+        그렇게 국비훈련을 마치고 구인을하다가 킨스데이즈라는 기업에 컨택을해서<br>
+        직원개념이아닌 프리랜서로 프로젝트를 받아서 작업하는 형식으로 일을 시작하게되었습니다.
     </div>
-</div>
-<div class="resumeSection1">
-    <h2>자기소개</h2>
-    <hr style="margin:20px 0px;">
-    <h2>코딩?</h2>
-    내가 원하는 것을만들어 줄 수 있게 해준 또 하나의 손 같은 존재입니다.<br>
-    에디터를 사용해서 내가 원하는 작업물을 시각적이 아닌 읽어서 만들어 낸다는 느낌은 저한테 정말 좋았습니다.<br>
-    컴퓨터를 좋아하는 저한테 적성도 정말 잘 맞았고 그런지 앉아서 몇 시간을 코딩을 해도 지루하지 않고<br>
-    이것저것 만져보면서 오류를 해결했을 때 그 기분은 매우 좋아서 저는 코딩을 놓지 못하겠습니다.<br>
-    지금의 계획은 Javascript를 좀더 숙련도있게 계속해서 프런트, 백앤드 모두를 움직일수있고 생각대로<br>
-    만들어낼수 있는 개발자까지 성장하고싶은 한 사람입니다.<br>
-    <br>
-    그럼 제가 코딩을 좋아하게된<br>
-    저의 짧은 인생사를 소개해볼께요!<br>
-    저의 나이는 95년생입니다. 어리다면 어릴수있지만 자신에 대해서 책임을 져야할 나이라고 생각합니다<br>
-    20살까지는 어느 목표가없는 평범한 학생이었습니다. 대학교진학은 학비의 부담을 느껴 포기하고 빠른 취업을 생각하고있었죠<br>
-    그렇게 21살에 입대를하고 군대에서 많은 것들이 저를 성숙하게 만들어줬습니다.<br>
-    22살 겨울에 재대를하고 저는 UIUX디자인(국비훈련) 받게되었습니다.<br>
-    디자인을 고등학교 방송부때부터 해왔는데 그 디자인을 텍스트로 하나하나 만드는 재미<br>
-    블로그를 운영하는 것에있어서 엄청난 확장성을 느껴서<br>
-    그때부터 웹개발자가 되야겠다라고 마음을 먹었습니다.<br>
-    <br>
-    그렇게 국비훈련을 마치고 구인을하다가 킨스데이즈라는 기업에 컨택을해서<br>
-    직원개념이아닌 프리랜서로 프로젝트를 받아서 작업하는 형식으로 일을 시작하게되었습니다.
-</div>
-<div class="resumeSection3">
-    <h2>경력사항 / 진행한 프로젝트</h2>
-    <hr>
-    <ul class="timeline">
-        <li>
-            <p class="timeline-date">2017년5월</p>
-            <div class="timeline-content">
-                <div class="projectName">
-                    2017.05 ~ 2017.12<br>
-                    <b>UXUI디자인</b><br>
-                    웹/앱에 관한 UIUX디자인 국비훈련 이수를 완료하였습니다.<br>
-                </div>
-            </div>
-        </li>
-        <li>
-            <p class="timeline-date">2018년3월</p>
-            <div class="timeline-content">
-                <div class="projectName">
-                    2018.03 ~ 2018.11<br>
-                    <b>킨스데이즈</b><br>
-                    프리랜서<br>
-                </div>
-                <div class="innerProject">
+    <div class="resumeSection3">
+        <h2>경력사항 / 진행한 프로젝트</h2>
+        <hr>
+        <ul class="timeline">
+            <li>
+                <p class="timeline-date">2017년5월</p>
+                <div class="timeline-content">
                     <div class="projectName">
-                        2018.03<br>
-                        <b><a target="_blank" href="http://blog.mbc.co.kr/" rel="noopener noreferrer"><i class="fas fa-link"></i> MBC blog</a></b><br>
-                        Tistory 기반의 테마를 수정하는 작업을 하였습니다.<br>
-                    </div>
-                    <div class="projectName">
-                        2018.04<br>
-                        <b><a target="_blank" href="http://marblerocket.com" rel="noopener noreferrer"><i class="fas fa-link"></i> Marble roket</a></b><br>
-                        카페24의 서버에 워드프레스기반의 사이트 구현 / 기본쇼핑몰<br>
-                    </div>
-                    <div class="projectName">
-                        2018.05<br>
-                        <b><a target="_blank" href="http://lkavenue.com" target="_blank" rel="noopener noreferrer"><i class="fas fa-link"></i> LK avenue</a></b><br>
-                        서버설치, 워드프레스기반 사이트 구현 / 오픈마켓기능 핵심 / 소셜로그인<br>
-                    </div>
-                    <div class="projectName">
-                        2018.06<br>
-                        <b><a target="_blank" href="https://brooklynpetco.com/" target="_blank" rel="noopener noreferrer"><i class="fas fa-link"></i> brooklynpetco</a></b><br>
-                        서버설치, 워드프레스기반 사이트 구현 / 정기결제기능 핵심<br>
-                    </div>
-                    <div class="projectName">
-                        2018.07<br>
-                        <b><a target="_blank" href="https://petnature.kr" rel="noopener noreferrer"><i class="fas fa-link"></i> 펫네이처</a></b><br>
-                        서버이전 개별페이지별 수정 및 퍼블리싱 / 정기결제기능 핵심<br>
-                    </div>
-                    <div class="projectName">
-                        2018.09<br>
-                        <b><a target="_blank" href="https://thetonade.com/" rel="noopener noreferrer"><i class="fas fa-link"></i> Thetonade</a></b><br>
-                        카페24의 서버에서 워드프레스기반의 사이트 구현 / 정기결제기능 핵심<br>
-                        작업내용 : <a target="_blank" href="https://docs.google.com/document/d/1ihkjRWuCLa0_NmTl_XRnTKjO065Qja-uLD7u5YYP1oE/edit?usp=sharing" rel="noopener noreferrer">Google
-                            docs</a>
-                    </div>
-                    <div class="projectName">
-                        2018.11<br>
-                        <b><a target="_blank" href="http://kalascoffee.com" rel="noopener noreferrer"><i class="fas fa-link"></i> Kalascoffee</a></b><br>
-                        기존사이트 리뉴얼 / 퍼블리싱 핵심<br>
-                        작업내용 : <a target="_blank" href="https://docs.google.com/document/d/1x6DYbYo_KTonoigJzEtbjpRDw3JBingewxJocYWcMWg/edit?usp=sharing" rel="noopener noreferrer">Google
-                            docs</a>
+                        2017.05 ~ 2017.12<br>
+                        <b>UXUI디자인</b><br>
+                        웹/앱에 관한 UIUX디자인 국비훈련 이수를 완료하였습니다.<br>
                     </div>
                 </div>
-            </div>
-        </li>
-        <li>
-            <p class="timeline-date">2018년12월</p>
-            <div class="timeline-content">
-                <div class="projectName">
-                    2018.12.12 ~ <br>
-                    <b>Peterosea</b><br>
-                    publisher<br>
-                </div>
-                <div class="innerProject">
+            </li>
+            <li>
+                <p class="timeline-date">2018년3월</p>
+                <div class="timeline-content">
                     <div class="projectName">
-                        2018.12<br>
-                        <b><a target="_blank" href="http://buildingpoint.co.kr" rel="noopener noreferrer"><i class="fas fa-link"></i> buildingpoint</a></b><br>
-                        Divi 기반을둔 퍼블리싱작업 경험
+                        2018.03 ~ 2018.11<br>
+                        <b>킨스데이즈</b><br>
+                        프리랜서<br>
+                    </div>
+                    <div class="innerProject">
+                        <div class="projectName">
+                            2018.03<br>
+                            <b><a target="_blank" href="http://blog.mbc.co.kr/" rel="noopener noreferrer"><i class="fas fa-link"></i> MBC blog</a></b><br>
+                            Tistory 기반의 테마를 수정하는 작업을 하였습니다.<br>
+                        </div>
+                        <div class="projectName">
+                            2018.04<br>
+                            <b><a target="_blank" href="http://marblerocket.com" rel="noopener noreferrer"><i class="fas fa-link"></i> Marble roket</a></b><br>
+                            카페24의 서버에 워드프레스기반의 사이트 구현 / 기본쇼핑몰<br>
+                        </div>
+                        <div class="projectName">
+                            2018.05<br>
+                            <b><a target="_blank" href="http://lkavenue.com" target="_blank" rel="noopener noreferrer"><i class="fas fa-link"></i> LK avenue</a></b><br>
+                            서버설치, 워드프레스기반 사이트 구현 / 오픈마켓기능 핵심 / 소셜로그인<br>
+                        </div>
+                        <div class="projectName">
+                            2018.06<br>
+                            <b><a target="_blank" href="https://brooklynpetco.com/" target="_blank" rel="noopener noreferrer"><i class="fas fa-link"></i> brooklynpetco</a></b><br>
+                            서버설치, 워드프레스기반 사이트 구현 / 정기결제기능 핵심<br>
+                        </div>
+                        <div class="projectName">
+                            2018.07<br>
+                            <b><a target="_blank" href="https://petnature.kr" rel="noopener noreferrer"><i class="fas fa-link"></i> 펫네이처</a></b><br>
+                            서버이전 개별페이지별 수정 및 퍼블리싱 / 정기결제기능 핵심<br>
+                        </div>
+                        <div class="projectName">
+                            2018.09<br>
+                            <b><a target="_blank" href="https://thetonade.com/" rel="noopener noreferrer"><i class="fas fa-link"></i> Thetonade</a></b><br>
+                            카페24의 서버에서 워드프레스기반의 사이트 구현 / 정기결제기능 핵심<br>
+                            작업내용 : <a target="_blank" href="https://docs.google.com/document/d/1ihkjRWuCLa0_NmTl_XRnTKjO065Qja-uLD7u5YYP1oE/edit?usp=sharing" rel="noopener noreferrer">Google
+                                docs</a>
+                        </div>
+                        <div class="projectName">
+                            2018.11<br>
+                            <b><a target="_blank" href="http://kalascoffee.com" rel="noopener noreferrer"><i class="fas fa-link"></i> Kalascoffee</a></b><br>
+                            기존사이트 리뉴얼 / 퍼블리싱 핵심<br>
+                            작업내용 : <a target="_blank" href="https://docs.google.com/document/d/1x6DYbYo_KTonoigJzEtbjpRDw3JBingewxJocYWcMWg/edit?usp=sharing" rel="noopener noreferrer">Google
+                                docs</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </li>
-    </ul>
-</div>
-<div class="resumeSection4">
-    <p class="slogan">사계절이라는 거친 세상에서도 꾸준함으로 무럭무럭 성장할 식물같은 사람이 되자.</p>
-</div>
-<span class="editCommit">update : 2018.12.22</span><br>
-<span class="editCommit up">제작 : 2018.12.04</span>
-
+            </li>
+            <li>
+                <p class="timeline-date">2018년12월</p>
+                <div class="timeline-content">
+                    <div class="projectName">
+                        2018.12.12 ~ <br>
+                        <b>Peterosea</b><br>
+                        publisher<br>
+                    </div>
+                    <div class="innerProject">
+                        <div class="projectName">
+                            2018.12<br>
+                            <b><a target="_blank" href="http://buildingpoint.co.kr" rel="noopener noreferrer"><i class="fas fa-link"></i> buildingpoint</a></b><br>
+                            Divi 기반을둔 퍼블리싱작업 경험
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="resumeSection4">
+        <p class="slogan">사계절이라는 거친 세상에서도 꾸준함으로 무럭무럭 성장할 식물같은 사람이 되자.</p>
+    </div>
+    <span class="editCommit">update : 2018.12.22</span><br>
+    <span class="editCommit up">제작 : 2018.12.04</span>
+</section>
+<section class="green-ani">
+    <div id="canvas_container_div">
+        <canvas id="canvas" width="1000" height="1000"></canvas> <!--(large size for higher resolution)-->
+    </div>
+</section>
+<script type="text/javascript" src="/00-js/resume-01.js"></script>
+<link rel = "stylesheet" type = "text/css" href = "/00-css/resume.css" />
+<link rel="stylesheet" type="text/css" href="/00-css/time-line.css">
 <?php
 get_footer();
