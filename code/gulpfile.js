498a29a6e222178710ae44f48c1c1efab52f5db8
Dan9509 <dandan9509@gmail.com>
2019-03-29 08:45:54 +0000

    chagne syntax gulp 3.0 > 4.0

--- code/gulpfile.js
+++ code/gulpfile.js
@@ -5,29 +5,33 @@ var pug = require('gulp-pug');
 var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
 
-// 일반 컴파일
-gulp.task('sass', function () {
-    return gulp.src('./scss/style.min.scss') // 입력 경로
+// gulp 4.0 변환
+
+// 통합 scss
+function sass_integrated(){
+    return gulp.src('./scss/intergrated/style.min.scss') // 입력 경로
         .pipe(sass({
             outputStyle: 'compressed'
         }).on('error', sass.logError))
         .pipe(gulp.dest('../')); // 출력 경로
-});
-
-// 런타임 중 파일 감시
-gulp.task('sass:watch', function () {
-    gulp.watch('./scss/*.scss', gulp.series('sass')); // 입력 경로와 파일 변경 감지 시 실행할 Actions(Task Name)
-});
-
-// Babel run!
-gulp.task('babel', function () {
+}
+// 분리형 scss
+function sass_container(){
+    return gulp.src('./scss/container/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+}
+function babel(){
     return gulp.src('./Babel/*.js')
         .pipe(sourcemaps.init())
         .pipe(babel())
         .pipe(sourcemaps.write('./Babel/map/',{sourcRoot: '../src'}))
         .pipe(gulp.dest('./js/Babel/'));
-});
+}
 
-gulp.task('babel:watch', function(){
-    gulp.watch('./Babel/*.js', gulp.series('babel'));
+// watch
+gulp.task('hello', function(){
+    gulp.watch('./scss/intergrated/*.scss', gulp.series(gulp.parallel(sass_integrated)));
+    gulp.watch('./scss/container/*.scss', gulp.series(gulp.parallel(sass_container)));
+    gulp.watch('./Babel/*.js', gulp.series(babel));
 });
