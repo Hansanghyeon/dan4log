18b0dab4742d61b53ae45c91e15da50cceebfe3f
Dan9509 <dandan9509@gmail.com>
2019-04-03 05:36:05 +0000

    gulp-typescript setting

--- code/gulpfile.js
+++ code/gulpfile.js
@@ -4,6 +4,7 @@ var sass = require('gulp-sass');
 var pug = require('gulp-pug');
 var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
+var ts = require('gulp-typescript');
 
 // gulp 4.0 변환
 
@@ -21,6 +22,8 @@ function sass_container(){
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('./css/'));
 }
+
+// Babel
 function babel(){
     return gulp.src('./Babel/*.js')
         .pipe(sourcemaps.init())
@@ -29,9 +32,18 @@ function babel(){
         .pipe(gulp.dest('./js/Babel/'));
 }
 
+// TypeScript
+function typescript(){
+    return gulp.src('./TypeScript/*.ts')
+        .pipe(ts())
+        .pipe(gulp.dest('./js/TypeScript/'));
+}
+
+
 // watch
 gulp.task('hello', function(){
     gulp.watch('./scss/intergrated/*.scss', gulp.series(gulp.parallel(sass_integrated)));
     gulp.watch('./scss/container/*.scss', gulp.series(gulp.parallel(sass_container)));
     gulp.watch('./Babel/*.js', gulp.series(babel));
+    gulp.watch('./TypeScript/*.ts', gulp.series(typescript));
 });
