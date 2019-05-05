5b51dfcb74238dca9b5f61f94c251d949b30881c
Dan9509 <dandan9509@gmail.com>
2019-05-05 02:51:58 +0000

    관계자 로그인일때 add body class, dash board class 추가

--- functions.php
+++ functions.php
@@ -265,3 +265,41 @@ function githubHpyerlink(){
         return $github;
 }
 add_shortcode('github_link','githubHpyerlink');
+
+// admin bashboard body class
+function my_admin_body_class( $classes ) {
+    return "$classes dan_admin_dashborad";
+    // Or: return "$classes my_class_1 my_class_2 my_class_3";
+}
+add_filter( 'admin_body_class', 'my_admin_body_class' );
+
+
+// admin filter sites body class
+function print_user_classes() {
+    if ( is_user_logged_in() ) {
+        add_filter('body_class','class_to_body');
+        add_filter('admin_body_class', 'class_to_body_admin');
+    }
+}
+add_action('init', 'print_user_classes');
+
+/// Add user role class to front-end body tag
+function class_to_body($classes) {
+    global $current_user;
+    $user_role = array_shift($current_user->roles);
+    $classes[] = $user_role.' ';
+    return $classes;
+}
+
+/// Add user role class and user id to front-end body tag
+
+// add 'class-name' to the $classes array
+function class_to_body_admin($classes) {
+    global $current_user;
+    $user_role = array_shift($current_user->roles);
+    /* Adds the user id to the admin body class array */
+    $user_ID = $current_user->ID;
+    $classes = $user_role.' '.'user-id-'.$user_ID ;
+    return $classes;
+    return 'user-id-'.$user_ID;
+}
