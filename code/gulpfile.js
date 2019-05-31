59fc926aab455a87f55cb190e1b9a60ad61bb87f
Dan9509 <dandan9509@gmail.com>
2019-05-31 16:10:33 +0900

    스크럼? 정리, sass오류 슬랙ㅌ으로 메세지보내기 구현

--- code/gulpfile.js
+++ code/gulpfile.js
@@ -4,10 +4,19 @@ const   gulp = require('gulp');
 const   sass = require('gulp-sass'),
         pug = require('gulp-pug'),
         sourcemaps = require('gulp-sourcemaps'),
-        babel = require('gulp-babel'),
+        bb = require('gulp-babel'),
         ts = require('gulp-typescript'),
         autoprefixer = require('gulp-autoprefixer'),
         rename = require('gulp-rename');
+
+// gulp notice plugin Slack
+const   slack = require('gulp-slack')({
+            url: 'https://hooks.slack.com/services/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',
+            channel: 'dan_ec2',    // Optional
+            user: 'Sass',       // Optional
+            icon_url: 'https://cdn.4log.dev/icon/sass-1024x1024.png',   // Optional
+            icon_emoji: ''  // Optional
+        });
 
 
 // gulp 4.0 변환
@@ -17,8 +26,27 @@ function sass_integrated(){
     return gulp
         .src('./scss/intergrated/style.min.scss')               // 입력 경로
         .pipe(sourcemaps.init())                                // 소스맵
-        .pipe(sass({ outputStyle: 'compressed' })               // minify
-        .on('error', sass.logError))                            // log
+        .pipe(
+            sass({ outputStyle: 'compressed' })
+            .on(
+                'error', function (err) {
+                    slack([
+                        {
+                            'text' : '에러발생' ,
+                            'color': '#da1836',
+                            'fields': [
+                                {
+                                    'title': 'dan4log',
+                                    'value': err.message.toString()
+                                }
+                            ]
+                        }
+                    ]);
+                    console.log(err.message.toString());
+                    this.emit('end');
+                }
+            )
+        )
         .pipe(sourcemaps.write('/map',{sourcRoot: '.'}))        // 소스맵 경로 주석첨부
         .pipe(gulp.dest('../public/css/'));                     // 출력경로
 }
@@ -27,8 +55,10 @@ function sass_container(){
     return gulp
         .src('./scss/container/*.scss')                         // 입력경로
         .pipe(sourcemaps.init())                                // 소스맵
-        .pipe(sass({outputStyle: 'compressed'})                 // minify
-        .on('error', sass.logError))                            // log
+        .pipe(
+            sass({outputStyle: 'compressed'})                   // minify
+            .on('error', sass.logError)                         // log
+        )
         .pipe(sourcemaps.write('/map',{sourcRoot: '.'}))        // 소스맵경로 주석첨부
         .pipe(gulp.dest('../public/css/'));                     // 출력경로
 }
@@ -38,7 +68,7 @@ function babel(){
     return gulp
         .src('./Babel/*.js')                                    // 입력경로
         .pipe(sourcemaps.init())                                // 소스맵
-        .pipe(babel())                                          // complie
+        .pipe(bb())                                          // complie
         .pipe(sourcemaps.write('/map/',{sourcRoot: '.'}))       // 소스맵경로 주석첨부
         .pipe(gulp.dest('../public/js/'));                      // 출력경로
 }
@@ -62,7 +92,6 @@ function cross_browser(){
         .pipe(rename('style.min.prefix.css'))                   // 출력이름 변경
         .pipe(gulp.dest('../public/css/'));                     // 출력경로
 }
-
 
 // watch
 gulp.task('hello', function(){
