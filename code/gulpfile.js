d5549745c9dca36f430999ca64c2018560504acd
Dan9509 <dandan9509@gmail.com>
2019-06-16 16:04:39 +0900

    change dev setting wp_develop_space

--- code/gulpfile.js
+++ code/gulpfile.js
@@ -1,42 +1,79 @@
 // gulpfile.js
-const   gulp = require('gulp');
+const gulp = require('gulp');
+
+// 프로젝트명
+const PROJECT = 'dan4log';
+
+// cdn url
+var CDN_URL = 'https://cdn.4log.dev';
+
 // gulp plugin
-const   sass = require('gulp-sass'),
-        pug = require('gulp-pug'),
-        sourcemaps = require('gulp-sourcemaps'),
-        bb = require('gulp-babel'),
-        ts = require('gulp-typescript'),
-        autoprefixer = require('gulp-autoprefixer'),
-        rename = require('gulp-rename');
+const   sass 			= require('gulp-sass'),
+        pug 			= require('gulp-pug'),
+        sourcemaps 		= require('gulp-sourcemaps'),
+        bb 				= require('gulp-babel'),
+        ts 				= require('gulp-typescript'),
+        autoprefixer	= require('gulp-autoprefixer'),
+        rename 			= require('gulp-rename'),
+        awspublish      = require('gulp-awspublish'),
+        key             = require('./secret/key.js');
 
 // gulp notice plugin Slack
-const   slack = require('gulp-slack')({
-            url: 'https://hooks.slack.com/services/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',
-            channel: 'dan_ec2',    // Optional
-            user: 'Sass',       // Optional
-            icon_url: 'https://cdn.4log.dev/icon/sass-1024x1024.png',   // Optional
-            icon_emoji: ''  // Optional
-        });
+// webhook url list
+const slack_dataset = {
+    'space' : 'https://hooks.slack.com/services/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',
+    'icon_url' : {
+        'sass' : CDN_URL+'/icons/sass.png',
+        'babel' : CDN_URL+'/icons/babel.png',
+        'typescript' : CDN_URL+'/icons/typescript.png'
+    }
+}
+function slack_notice(user, channel, url, icon_url){
+    var slack = require('gulp-slack')({
+        url : url,
+        channel : channel,
+        user : user,
+        icon_url : icon_url
+    });
 
+    return slack;
+}
+
+// gulp s3 upload
+const   publisher = awspublish.create(
+    {
+        region: 'ap-northeast-2', 
+        params: {
+            Bucket: key.BUCKET_NAME.NAME
+        },
+        "accessKeyId": key.BUCKET_NAME.ACCESSKEYID,
+        "secretAccessKey": key.BUCKET_NAME.SECRETACCESSKEY
+    }
+)
+var headers = {
+    "Cache-Control": "max-age=315360000, no-transform, public"
+}
 
 // gulp 4.0 변환
 
 // 통합 scss
 function sass_integrated(){
     return gulp
-        .src('./scss/intergrated/style.min.scss')               // 입력 경로
-        .pipe(sourcemaps.init())                                // 소스맵
+        .src('./Scss/mix/style.min.scss')
+        // 해당파일 소스맵생성
+        .pipe(sourcemaps.init())
+        // slick notice
         .pipe(
             sass({ outputStyle: 'compressed' })
             .on(
                 'error', function (err) {
-                    slack([
+                    slack_notice('Sass', '', slack_dataset.space, slack_dataset.icon_url.sass)([
                         {
-                            'text' : '에러발생' ,
-                            'color': '#da1836',
+                            'text' : PROJECT ,
+                            'color': '#ec407a',
                             'fields': [
                                 {
-                                    'title': 'dan4log',
+                                    'title': '에러발생 | mix',
                                     'value': err.message.toString()
                                 }
                             ]
@@ -47,56 +84,158 @@ function sass_integrated(){
                 }
             )
         )
-        .pipe(sourcemaps.write('/map',{sourcRoot: '.'}))        // 소스맵 경로 주석첨부
-        .pipe(gulp.dest('../public/css/'));                     // 출력경로
+        // source map 경로 css 마지막 추가
+        .pipe(sourcemaps.write('/map',{sourcRoot: '.'}))
+        // 소스맵할당 개발용 min파일
+        .pipe(rename('style.min.dev.css'))
+        // output
+        .pipe(gulp.dest('../public/css/'))
+        // s3 upload
+        .pipe(rename(function(path){
+            path.dirname = PROJECT + '/css/' + path.dirname;
+        }))
+        .pipe(publisher.publish(headers))
+        // .pipe(publisher.cache())
+        .pipe(awspublish.reporter());
 }
 // 분리형 scss
 function sass_container(){
     return gulp
-        .src('./scss/container/*.scss')                         // 입력경로
-        .pipe(sourcemaps.init())                                // 소스맵
+        .src('./Scss/single/*.scss')
+        // 해당파일 소스맵생성
+        .pipe(sourcemaps.init())
+        // slick notice
         .pipe(
-            sass({outputStyle: 'compressed'})                   // minify
-            .on('error', sass.logError)                         // log
+            sass({ outputStyle: 'compressed' })
+            .on(
+                'error', function (err) {
+                    slack_notice('Sass', '', slack_dataset.space, slack_dataset.icon_url.sass)([
+                        {
+                            'text' : PROJECT ,
+                            'color': '#ec407a',
+                            'fields': [
+                                {
+                                    'title': '에러발생 | single',
+                                    'value': err.message.toString()
+                                }
+                            ]
+                        }
+                    ]);
+                    console.log(err.message.toString());
+                    this.emit('end');
+                }
+            )
         )
-        .pipe(sourcemaps.write('/map',{sourcRoot: '.'}))        // 소스맵경로 주석첨부
-        .pipe(gulp.dest('../public/css/'));                     // 출력경로
+        // source map 경로 css 마지막 추가
+        .pipe(sourcemaps.write('/map',{sourcRoot: '.'}))
+        // output
+        .pipe(gulp.dest('../public/css/'))
+        // s3upload
+        .pipe(rename(function(path){
+            path.dirname = PROJECT + '/css/' + path.dirname;
+        }))
+        .pipe(publisher.publish(headers))
+        // .pipe(publisher.cache())
+        .pipe(awspublish.reporter());
 }
 
 // Babel
 function babel(){
     return gulp
-        .src('./Babel/*.js')                                    // 입력경로
-        .pipe(sourcemaps.init())                                // 소스맵
-        .pipe(bb())                                          // complie
-        .pipe(sourcemaps.write('/map/',{sourcRoot: '.'}))       // 소스맵경로 주석첨부
-        .pipe(gulp.dest('../public/js/Babel/'));                      // 출력경로
+        .src('./Babel/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(
+            bb()
+            .on(
+                'error', function(err){
+                    slack_notice('Babel', '', slack_dataset.space, slack_dataset.icon_url.babel)([
+                        {
+                            'text' : PROJECT ,
+                            'color': '#fdd835',
+                            'fields': [
+                                {
+                                    'title': '에러발생 | Babel',
+                                    'value': err.message.toString()
+                                }
+                            ]
+                        }
+                    ]);
+                    console.log(err.message.toString());
+                    this.emit('end');
+                }
+            )
+        )
+        .pipe(sourcemaps.write('/map/',{sourcRoot: '.'}))
+        .pipe(gulp.dest('../public/js/'))
+        // s3upload
+        .pipe(rename(function(path){
+            path.dirname = PROJECT + '/js/' + path.dirname;
+        }))
+        .pipe(publisher.publish(headers))
+        // .pipe(publisher.cache())
+        .pipe(awspublish.reporter());
 }
 
 // TypeScript
 function typescript(){
     return gulp
-        .src('./TypeScript/*.ts')                               // 입력경로
-        .pipe(ts())                                             // complie  
-        .pipe(gulp.dest('../public/js/TypeScript/'));           // 출력경로
+		.src('./TypeScript/*.ts')
+		.pipe(sourcemaps.init())
+        .pipe(
+            ts()
+            .on(
+                'error', function(err){
+                    slack_notice('Typescript', '', slack_dataset.space, slack_dataset.icon_url.typescript)([
+                        {
+                            'text' : PROJECT ,
+                            'color': '#0288d1',
+                            'fields': [
+                                {
+                                    'title': '에러발생 | Typescript',
+                                    'value': err.message.toString()
+                                }
+                            ]
+                        }
+                    ]);
+                    console.log(err.message.toString());
+                    this.emit('end');
+                }
+            )
+        )
+		.pipe(sourcemaps.write('/map/',{sourcRoot: '.'}))
+        .pipe(gulp.dest('../public/js/'))
+        // s3upload
+        .pipe(rename(function(path){
+            path.dirname = PROJECT + '/js/' + path.dirname;
+        }))
+        .pipe(publisher.publish(headers))
+        // .pipe(publisher.cache())
+        .pipe(awspublish.reporter());
 }
 
 // Crossbrowser
 function cross_browser(){
     return gulp
-        .src('../public/css/style.min.css')                     // 입력경로
-        .pipe(autoprefixer({                                    // complie & option
+        .src('../public/css/style.min.dev.css')
+        .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(rename('style.min.prefix.css'))                   // 출력이름 변경
-        .pipe(gulp.dest('../public/css/'));                     // 출력경로
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('../public/css/'))
+        // s3upload
+        .pipe(rename(function(path){
+            path.dirname = PROJECT + '/css/' + path.dirname;
+        }))
+        .pipe(publisher.publish(headers))
+        // .pipe(publisher.cache())
+        .pipe(awspublish.reporter());
 }
 
 // watch
 gulp.task('hello', function(){
-    gulp.watch('./scss/intergrated/*.scss', gulp.series(gulp.parallel(sass_integrated),cross_browser));
-    gulp.watch('./scss/container/*.scss', gulp.series(gulp.parallel(sass_container)));
+    gulp.watch('./Scss/mix/*.scss', gulp.series(gulp.parallel(sass_integrated),cross_browser));
+    gulp.watch('./Scss/single/*.scss', gulp.series(gulp.parallel(sass_container)));
     gulp.watch('./Babel/*.js', gulp.series(babel));
     gulp.watch('./TypeScript/*.ts', gulp.series(typescript));
 });
