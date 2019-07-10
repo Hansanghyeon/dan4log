895a1180a656887c994f800fa2d08f2b88a2e1bc
Hansanghyeon <dandan9509@gmail.com>
2019-07-11 08:23:09 +0900

    Add: category notion style! interaction

---
+++ code/Babel/index.js
@@ -0,0 +1,19 @@
+let _diviLib_thumOff_sideOn = document.querySelector(".diviLib_thumOff_sideOn");
+let cover = document.querySelector(".category-hero-section");
+let cover_meta = document.querySelector(".category-hero-section meta");
+let content_wrap_title = document.querySelector(".no-hero");
+
+if (_diviLib_thumOff_sideOn) {
+  let _open = () => {
+    cover.classList += " open";
+    cover.style.backgroundImage =
+      "url('" + cover_meta.getAttribute("cover-bg-data") + "')";
+    content_wrap_title.style.display = "none";
+  };
+
+  let _close = () => {
+    content_wrap_title.style.display = "block";
+  };
+
+  cover_meta.getAttribute("cover") == "open" ? _open() : _close();
+}
